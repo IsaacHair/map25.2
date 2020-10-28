ed99ac3c0c468d199886856444ed31404cb7831a
Isaac Hair <isaacmhair@gmail.com>
2020-10-28 01:12:43 -0700

    made a dumb mistake, but its still not working even after fixing said mistake

--- vt0.c
+++ vt0.c
@@ -49,23 +49,41 @@ void comm4dat(int a, int b, int c, int d, int e) {
 	inst("imm out0 0400");
 }
 
-void jump(int mark) {
+void to(int mark) {
 	fprintf(fd, "%04x nc noop 0000 %04x\n", addr, mark);
 	addr++;
 }
 
+void rst() {
+	fprintf(fd, "%04x imm out0 1000 %04x\n", addr, addr%2 ? addr+2 : addr+1);
+	addr += addr%2 ? 2 : 1;
+	fprintf(fd, "%04x in jzor 0001 %04x\n", addr, addr+1);
+	addr++;
+	fprintf(fd, "%04x in noop 0000 %04x\n", addr, addr-1);
+	addr++;
+	fprintf(fd, "%04x imm out1 1000 %04x\n", addr, addr+2);
+	addr += 2;
+	fprintf(fd, "%04x in jzor 0001 %04x\n", addr, addr+1);
+	addr++;
+	fprintf(fd, "%04x in noop 0000 %04x\n", addr, addr+2);
+	addr++;
+	fprintf(fd, "%04x in noop 0000 %04x\n", addr, addr-2);
+	addr++;
+}
+
 void main() {
-	fd = fopen("vma412_test_0.a25", "w");
+	fd = fopen("vt0.a25", "w");
 	addr = 0;
 	int placeholder;
 	inst("imm dir1 ffff");
 	inst("imm out1 ffff");
-	//rst(); //if I am correct, this is unnecessary
+	rst(); //reset
 	inst("imm out0 0c00"); //cs and rs low
 	buswrite(0x38); //out of idle
 	buswrite(0x11); //out of sleep
-	buswrite(0x13); //display is on
+	buswrite(0x13); //normal display mode
 	buswrite(0x20); //inversion is off
+	buswrite(0x29); //display is on
 	comm1dat(0x0c, 0xe5); //set COLMOD
 	comm4dat(0x2a, 0x00, 0x00, 0x00, 0xef); //set column min-max
 	comm4dat(0x2b, 0x00, 0x00, 0x01, 0x3f); //set page min-max
@@ -75,6 +93,6 @@ void main() {
 	buswrite(0x00); //red (upper 6 bits)
 	buswrite(0xc8); //green (upper 6 bits)
 	buswrite(0xd8); //blue (upper 6 bits)
-	jump(placeholder);
+	to(placeholder);
 	fclose(fd);
 }
