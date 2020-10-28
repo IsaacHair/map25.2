1a94b4b9221e6d3deda69526dcb34838763b3f3d
Isaac Hair <isaacmhair@gmail.com>
2020-10-27 23:59:43 -0700

    lcd.... yeah lets try to get it working

---
+++ lcd_arduino_tests/test6/test6.ino
@@ -0,0 +1,266 @@
+//
+// ILI9341 ID Checker against data sheet http://www.adafruit.com/datasheets/ILI9341.pdf
+// Ugly bit basher - R Jennings 11/11/14 (http://www.shaders.co.uk)
+// ACTUALLY YA BOI ISAAC COPIED THIS GUYS WORK, thanks man!
+//
+ #define CS A3   // Chip Select goes to Analog 3
+ #define CD A2   // Command/Data goes to Analog 2
+ #define WR A1   // LCD Write goes to Analog 1
+ #define RD A0   // LCD Read goes to Analog 0
+ #define RST A4  // Can alternately just connect to Arduino's reset pin
+ #define D0 8    // DO
+ #define D1 9    // D1
+ #define D2 2    // D2
+ #define D3 3    // D3
+ #define D4 4    // D4
+ #define D5 5    // D5
+ #define D6 6    // D6
+ #define D7 7    // D7
+
+void setup(void) {
+  int x;
+// Set up port directions and default states
+  pinMode(CS, OUTPUT);  pinMode(CD, OUTPUT);  pinMode(WR, OUTPUT);  pinMode(RD, OUTPUT);  pinMode(RST, OUTPUT);
+  digitalWrite(CS, 1); digitalWrite(CD, 1); digitalWrite(WR, 1); digitalWrite(RD, 1); digitalWrite(RST, 1);
+  pinMode(D0, OUTPUT); pinMode(D1, OUTPUT); pinMode(D2, OUTPUT); pinMode(D3, OUTPUT);
+  pinMode(D4, OUTPUT); pinMode(D5, OUTPUT); pinMode(D6, OUTPUT); pinMode(D7, OUTPUT);
+
+// This just drivers the serial monitor no screen activity 
+  Serial.begin(9600);
+  Serial.println("TFT LCD test - Read of the ID register");
+//HW Reset
+  digitalWrite(RST,0); delay(5);
+  digitalWrite(RST,1); delay(500);
+/*// Read the ID Register, D3h
+// Start by writing to the command register with the register to read
+///*********READING DISPLAY INFO NOW, so expected values are different
+  digitalWrite(CD, 0);
+  digitalWrite(CS, 0);
+  digitalWrite(D0, 1); digitalWrite(D1, 0); digitalWrite(D2, 0); digitalWrite(D3, 1);  // 9 - Change for lower nibble as required
+  digitalWrite(D4, 0); digitalWrite(D5, 0); digitalWrite(D6, 0); digitalWrite(D7, 0);  // 0 - Change for upper nibble as required
+  digitalWrite(WR, 0); digitalWrite(WR, 1);
+  digitalWrite(CS, 1);   
+// Switch to reading   
+  pinMode(D0, INPUT);    pinMode(D1, INPUT);    pinMode(D2, INPUT);    pinMode(D3, INPUT);
+  pinMode(D4, INPUT);    pinMode(D5, INPUT);    pinMode(D6, INPUT);    pinMode(D7, INPUT);
+  x=0;
+// Then read the four bytes and print to the serial port
+// See section 8.3.23 page 233 for read sequence
+// 1st Parameter - Dummy byte could be anything
+  digitalWrite(CD, 1);
+  digitalWrite(CS, 0);
+  digitalWrite(RD, 0);
+  x=0;
+  digitalWrite(RD, 0);
+  x|=digitalRead(D0);    x|=digitalRead(D1)<<1; x|=digitalRead(D2)<<2; x|=digitalRead(D3)<<3;
+  x|=digitalRead(D4)<<4; x|=digitalRead(D5)<<5; x|=digitalRead(D6)<<6; x|=digitalRead(D7)<<7;
+  Serial.print("Dummny Byte Read =        "); Serial.println(x, HEX);   
+  digitalWrite(RD, 1);
+ // 2nd Parameter should be 00h
+  x=0;
+  digitalWrite(RD, 0);
+  x|=digitalRead(D0);    x|=digitalRead(D1)<<1; x|=digitalRead(D2)<<2; x|=digitalRead(D3)<<3;
+  x|=digitalRead(D4)<<4; x|=digitalRead(D5)<<5; x|=digitalRead(D6)<<6; x|=digitalRead(D7)<<7;
+  Serial.print("2nd Parameter Byte Read = "); Serial.println(x, HEX);   
+  digitalWrite(RD, 1);
+ // 3rd Parameter should be 93h   
+  x=0;
+  digitalWrite(RD, 0);
+  x|=digitalRead(D0);    x|=digitalRead(D1)<<1;  x|=digitalRead(D2)<<2; x|=digitalRead(D3)<<3;
+  x|=digitalRead(D4)<<4; x|=digitalRead(D5)<<5;  x|=digitalRead(D6)<<6; x|=digitalRead(D7)<<7;
+  Serial.print("3rd Parameter Byte Read = "); Serial.println(x, HEX);   
+  digitalWrite(RD, 1);
+ // 4th Parameter should be 41h     
+  x=0;
+  digitalWrite(RD, 0);
+  x|=digitalRead(D0);    x|=digitalRead(D1)<<1; x|=digitalRead(D2)<<2; x|=digitalRead(D3)<<3;
+  x|=digitalRead(D4)<<4; x|=digitalRead(D5)<<5; x|=digitalRead(D6)<<6; x|=digitalRead(D7)<<7;
+  Serial.print("4th Parameter Byte Read = "); Serial.println(x, HEX);   
+  digitalWrite(RD, 1);
+  digitalWrite(CS, 1);*/
+
+  //set all parameters and get ready to write  
+  pinMode(D0, OUTPUT);    pinMode(D1, OUTPUT);    pinMode(D2, OUTPUT);    pinMode(D3, OUTPUT);
+  pinMode(D4, OUTPUT);    pinMode(D5, OUTPUT);    pinMode(D6, OUTPUT);    pinMode(D7, OUTPUT);
+  digitalWrite(RD, 1);
+  digitalWrite(CS, 1);
+  digitalWrite(WR, 1);
+  digitalWrite(CD, 1);
+
+  //out of IDLE mode
+  digitalWrite(CS, 0);
+  digitalWrite(CD, 0);
+  digitalWrite(D0, 0); digitalWrite(D1, 0); digitalWrite(D2, 0); digitalWrite(D3, 1);  // 8 - Change for lower nibble as required
+  digitalWrite(D4, 1); digitalWrite(D5, 1); digitalWrite(D6, 0); digitalWrite(D7, 0);  // 3 - Change for upper nibble as required
+  digitalWrite(WR, 0); digitalWrite(WR, 1);
+  digitalWrite(CD, 1);
+  digitalWrite(CS, 1);
+
+  //out of SLEEP mode
+  digitalWrite(CS, 0);
+  digitalWrite(CD, 0);
+  digitalWrite(D0, 1); digitalWrite(D1, 0); digitalWrite(D2, 0); digitalWrite(D3, 0);  // 1 - Change for lower nibble as required
+  digitalWrite(D4, 1); digitalWrite(D5, 0); digitalWrite(D6, 0); digitalWrite(D7, 0);  // 1 - Change for upper nibble as required
+  digitalWrite(WR, 0); digitalWrite(WR, 1);
+  digitalWrite(CD, 1);
+  digitalWrite(CS, 1);
+
+  //display on mode
+  digitalWrite(CS, 0);
+  digitalWrite(CD, 0);
+  digitalWrite(D0, 1); digitalWrite(D1, 1); digitalWrite(D2, 0); digitalWrite(D3, 0);  // 3 - Change for lower nibble as required
+  digitalWrite(D4, 1); digitalWrite(D5, 0); digitalWrite(D6, 0); digitalWrite(D7, 0);  // 1 - Change for upper nibble as required
+  digitalWrite(WR, 0); digitalWrite(WR, 1);
+  digitalWrite(CD, 1);
+  digitalWrite(CS, 1);
+
+  //set COLMOD
+  digitalWrite(CS, 0);
+  digitalWrite(CD, 0);
+  digitalWrite(D0, 0); digitalWrite(D1, 0); digitalWrite(D2, 1); digitalWrite(D3, 1);  // C - Change for lower nibble as required
+  digitalWrite(D4, 0); digitalWrite(D5, 0); digitalWrite(D6, 0); digitalWrite(D7, 0);  // 0 - Change for upper nibble as required
+  digitalWrite(WR, 0); digitalWrite(WR, 1);
+  digitalWrite(CD, 1);
+  digitalWrite(D0, 0); digitalWrite(D1, 1); digitalWrite(D2, 1); digitalWrite(D3, 0);  // 5 - Change for lower nibble as required
+  digitalWrite(D4, 0); digitalWrite(D5, 1); digitalWrite(D6, 1); digitalWrite(D7, 1);  // e - Change for upper nibble as required
+  digitalWrite(WR, 0); digitalWrite(WR, 1);
+  digitalWrite(CS, 1);
+
+  //invsersion off
+  digitalWrite(CS, 0);
+  digitalWrite(CD, 0);
+  digitalWrite(D0, 0); digitalWrite(D1, 0); digitalWrite(D2, 0); digitalWrite(D3, 0);  // 0 - Change for lower nibble as required
+  digitalWrite(D4, 0); digitalWrite(D5, 1); digitalWrite(D6, 0); digitalWrite(D7, 0);  // 2 - Change for upper nibble as required
+  digitalWrite(WR, 0); digitalWrite(WR, 1);
+  digitalWrite(CD, 1);
+  digitalWrite(CS, 1);
+
+  //turn memory to screen on
+  digitalWrite(CS, 0);
+  digitalWrite(CD, 0);
+  digitalWrite(D0, 1); digitalWrite(D1, 0); digitalWrite(D2, 0); digitalWrite(D3, 1);  // 9 - Change for lower nibble as required
+  digitalWrite(D4, 0); digitalWrite(D5, 1); digitalWrite(D6, 0); digitalWrite(D7, 0);  // 2 - Change for upper nibble as required
+  digitalWrite(WR, 0); digitalWrite(WR, 1);
+  digitalWrite(CD, 1);
+  digitalWrite(CS, 1);
+
+  //column address
+  digitalWrite(CS, 0);
+  digitalWrite(CD, 0);
+  digitalWrite(D0, 0); digitalWrite(D1, 1); digitalWrite(D2, 0); digitalWrite(D3, 1);  // A - Change for lower nibble as required
+  digitalWrite(D4, 0); digitalWrite(D5, 1); digitalWrite(D6, 0); digitalWrite(D7, 0);  // 2 - Change for upper nibble as required
+  digitalWrite(WR, 0); digitalWrite(WR, 1);
+  digitalWrite(CD, 1);
+  digitalWrite(D0, 0); digitalWrite(D1, 0); digitalWrite(D2, 0); digitalWrite(D3, 0);  // 0 - Change for lower nibble as required
+  digitalWrite(D4, 0); digitalWrite(D5, 0); digitalWrite(D6, 0); digitalWrite(D7, 0);  // 0 - Change for upper nibble as required
+  digitalWrite(WR, 0); digitalWrite(WR, 1);
+  digitalWrite(D0, 0); digitalWrite(D1, 0); digitalWrite(D2, 0); digitalWrite(D3, 0);  // 0 - Change for lower nibble as required
+  digitalWrite(D4, 0); digitalWrite(D5, 0); digitalWrite(D6, 0); digitalWrite(D7, 0);  // 0 - Change for upper nibble as required
+  digitalWrite(WR, 0); digitalWrite(WR, 1);
+  digitalWrite(D0, 0); digitalWrite(D1, 0); digitalWrite(D2, 0); digitalWrite(D3, 0);  // 0 - Change for lower nibble as required
+  digitalWrite(D4, 0); digitalWrite(D5, 0); digitalWrite(D6, 0); digitalWrite(D7, 0);  // 0 - Change for upper nibble as required
+  digitalWrite(WR, 0); digitalWrite(WR, 1);
+  digitalWrite(D0, 1); digitalWrite(D1, 1); digitalWrite(D2, 1); digitalWrite(D3, 1);  // f - Change for lower nibble as required
+  digitalWrite(D4, 0); digitalWrite(D5, 1); digitalWrite(D6, 1); digitalWrite(D7, 1);  // e - Change for upper nibble as required
+  digitalWrite(WR, 0); digitalWrite(WR, 1);
+  digitalWrite(CS, 1);
+
+  //page address
+  digitalWrite(CS, 0);
+  digitalWrite(CD, 0);
+  digitalWrite(D0, 1); digitalWrite(D1, 1); digitalWrite(D2, 0); digitalWrite(D3, 1);  // B - Change for lower nibble as required
+  digitalWrite(D4, 0); digitalWrite(D5, 1); digitalWrite(D6, 0); digitalWrite(D7, 0);  // 2 - Change for upper nibble as required
+  digitalWrite(WR, 0); digitalWrite(WR, 1);
+  digitalWrite(CD, 1);
+  digitalWrite(D0, 0); digitalWrite(D1, 0); digitalWrite(D2, 0); digitalWrite(D3, 0);  // 0 - Change for lower nibble as required
+  digitalWrite(D4, 0); digitalWrite(D5, 0); digitalWrite(D6, 0); digitalWrite(D7, 0);  // 0 - Change for upper nibble as required
+  digitalWrite(WR, 0); digitalWrite(WR, 1);
+  digitalWrite(D0, 0); digitalWrite(D1, 0); digitalWrite(D2, 0); digitalWrite(D3, 0);  // 0 - Change for lower nibble as required
+  digitalWrite(D4, 0); digitalWrite(D5, 0); digitalWrite(D6, 0); digitalWrite(D7, 0);  // 0 - Change for upper nibble as required
+  digitalWrite(WR, 0); digitalWrite(WR, 1);
+  digitalWrite(D0, 1); digitalWrite(D1, 0); digitalWrite(D2, 0); digitalWrite(D3, 0);  // 1 - Change for lower nibble as required
+  digitalWrite(D4, 0); digitalWrite(D5, 0); digitalWrite(D6, 0); digitalWrite(D7, 0);  // 0 - Change for upper nibble as required
+  digitalWrite(WR, 0); digitalWrite(WR, 1);
+  digitalWrite(D0, 1); digitalWrite(D1, 1); digitalWrite(D2, 1); digitalWrite(D3, 1);  // f - Change for lower nibble as required
+  digitalWrite(D4, 1); digitalWrite(D5, 1); digitalWrite(D6, 0); digitalWrite(D7, 0);  // 3 - Change for upper nibble as required
+  digitalWrite(WR, 0); digitalWrite(WR, 1);
+  digitalWrite(CS, 1);
+
+  //write that square to green
+  digitalWrite(CS, 0);
+  digitalWrite(CD, 0);
+  digitalWrite(D0, 0); digitalWrite(D1, 0); digitalWrite(D2, 1); digitalWrite(D3, 1);  // C - Change for lower nibble as required
+  digitalWrite(D4, 0); digitalWrite(D5, 1); digitalWrite(D6, 0); digitalWrite(D7, 0);  // 2 - Change for upper nibble as required
+  digitalWrite(WR, 0); digitalWrite(WR, 1);
+  digitalWrite(CD, 1);
+  for (int i = 0; i < 320; i++)
+    for (int j = 0; j < 240; j++) {
+      digitalWrite(D0, 0); digitalWrite(D1, 0); digitalWrite(D2, 0); digitalWrite(D3, 0);  // 0 - Change for lower nibble as required
+      digitalWrite(D4, 0); digitalWrite(D5, 0); digitalWrite(D6, 0); digitalWrite(D7, 0);  // 0 - Change for upper nibble as required
+      digitalWrite(WR, 0); digitalWrite(WR, 1);
+      digitalWrite(D0, 0); digitalWrite(D1, 0); digitalWrite(D2, 0); digitalWrite(D3, 1);  // 8 - Change for lower nibble as required
+      digitalWrite(D4, 0); digitalWrite(D5, 0); digitalWrite(D6, 1); digitalWrite(D7, 1);  // c - Change for upper nibble as required
+      digitalWrite(WR, 0); digitalWrite(WR, 1);
+      digitalWrite(D0, 0); digitalWrite(D1, 0); digitalWrite(D2, 0); digitalWrite(D3, 1);  // 8 - Change for lower nibble as required
+      digitalWrite(D4, 1); digitalWrite(D5, 1); digitalWrite(D6, 1); digitalWrite(D7, 1);  // f - Change for upper nibble as required
+      digitalWrite(WR, 0); digitalWrite(WR, 1);
+    }
+  digitalWrite(CD, 0);
+  digitalWrite(D0, 0); digitalWrite(D1, 0); digitalWrite(D2, 0); digitalWrite(D3, 0);  // 0 - Change for lower nibble as required
+  digitalWrite(D4, 0); digitalWrite(D5, 0); digitalWrite(D6, 0); digitalWrite(D7, 0);  // 0 - Change for upper nibble as required
+  digitalWrite(WR, 0); digitalWrite(WR, 1);
+  digitalWrite(CD, 1);
+  digitalWrite(CS, 1);
+
+  //read that first pixel
+  // Start by writing to the command register with the register to read
+  digitalWrite(CD, 0);
+  digitalWrite(CS, 0);
+  digitalWrite(D0, 0); digitalWrite(D1, 1); digitalWrite(D2, 1); digitalWrite(D3, 1);  // 3 - Change for lower nibble as required
+  digitalWrite(D4, 0); digitalWrite(D5, 1); digitalWrite(D6, 0); digitalWrite(D7, 0);  // 2 - Change for upper nibble as required
+  digitalWrite(WR, 0); digitalWrite(WR, 1);
+  digitalWrite(CS, 1);   
+// Switch to reading   
+  pinMode(D0, INPUT);    pinMode(D1, INPUT);    pinMode(D2, INPUT);    pinMode(D3, INPUT);
+  pinMode(D4, INPUT);    pinMode(D5, INPUT);    pinMode(D6, INPUT);    pinMode(D7, INPUT);
+// Then read the four bytes and print to the serial port
+// See section 8.3.23 page 233 for read sequence
+// 1st Parameter - Dummy byte could be anything
+  digitalWrite(CD, 1);
+  digitalWrite(CS, 0);
+  digitalWrite(RD, 0);
+  x=0;
+  digitalWrite(RD, 0);
+  x|=digitalRead(D0);    x|=digitalRead(D1)<<1; x|=digitalRead(D2)<<2; x|=digitalRead(D3)<<3;
+  x|=digitalRead(D4)<<4; x|=digitalRead(D5)<<5; x|=digitalRead(D6)<<6; x|=digitalRead(D7)<<7;
+  Serial.print("Dummny Byte Read =        "); Serial.println(x, HEX);   
+  digitalWrite(RD, 1);
+ // 2nd Parameter should be 00h
+  x=0;
+  digitalWrite(RD, 0);
+  x|=digitalRead(D0);    x|=digitalRead(D1)<<1; x|=digitalRead(D2)<<2; x|=digitalRead(D3)<<3;
+  x|=digitalRead(D4)<<4; x|=digitalRead(D5)<<5; x|=digitalRead(D6)<<6; x|=digitalRead(D7)<<7;
+  Serial.print("2nd Parameter Byte Read = "); Serial.println(x, HEX);   
+  digitalWrite(RD, 1);
+ // 3rd Parameter should be 93h   
+  x=0;
+  digitalWrite(RD, 0);
+  x|=digitalRead(D0);    x|=digitalRead(D1)<<1;  x|=digitalRead(D2)<<2; x|=digitalRead(D3)<<3;
+  x|=digitalRead(D4)<<4; x|=digitalRead(D5)<<5;  x|=digitalRead(D6)<<6; x|=digitalRead(D7)<<7;
+  Serial.print("3rd Parameter Byte Read = "); Serial.println(x, HEX);   
+  digitalWrite(RD, 1);
+ // 4th Parameter should be 41h     
+  x=0;
+  digitalWrite(RD, 0);
+  x|=digitalRead(D0);    x|=digitalRead(D1)<<1; x|=digitalRead(D2)<<2; x|=digitalRead(D3)<<3;
+  x|=digitalRead(D4)<<4; x|=digitalRead(D5)<<5; x|=digitalRead(D6)<<6; x|=digitalRead(D7)<<7;
+  Serial.print("4th Parameter Byte Read = "); Serial.println(x, HEX);   
+  digitalWrite(RD, 1);
+  digitalWrite(CS, 1);
+
+  Serial.print("so...\n");
+  
+}
+//Nothing to do here except ur mom but already did her so...
+void loop(void) {
+}
--- vma412_test_0.c
+++ vma412_test_0.c
@@ -20,14 +20,39 @@ FILE* fd;
 
 void inst(char*str) {
 	fprintf(fd, "%04x %s %04x\n", addr, str, addr+1);
-	addr+1;
+	addr++;
 }
 
-void comm(int val) {
-	char buff[100];
+void buswrite(int val) {
 	int i;
 	inst("imm out0 00ff");
-	for (i = 
+	fprintf(fd, "%04x imm out1 %04x %04x\n", addr, val%256, addr+1);
+	addr++;
+	inst("imm out0 0200");
+	inst("imm out1 0200");
+}
+
+void comm1dat(int a, int b) {
+	buswrite(a);
+	inst("imm out1 0400");
+	buswrite(b);
+	inst("imm out0 0400");
+}
+
+void comm4dat(int a, int b, int c, int d, int e) {
+	buswrite(a);
+	inst("imm out1 0400");
+	buswrite(b);
+	buswrite(c);
+	buswrite(d);
+	buswrite(e);
+	inst("imm out0 0400");
+}
+
+void jump(int mark) {
+	fprintf(fd, "%04x nc noop 0000 %04x\n", addr, mark);
+	addr++;
+}
 
 void main() {
 	fd = fopen("vma412_test_0.a25", "w");
@@ -35,20 +60,21 @@ void main() {
 	int placeholder;
 	inst("imm dir1 ffff");
 	inst("imm out1 ffff");
-	inst("imm out0 0800");
 	//rst(); //if I am correct, this is unnecessary
-	comm(0x38); //out of idle
-	comm(0x11); //out of sleep
-	comm(0x13); //display is on
-	comm(0x20); //inversion is off
+	inst("imm out0 0c00"); //cs and rs low
+	buswrite(0x38); //out of idle
+	buswrite(0x11); //out of sleep
+	buswrite(0x13); //display is on
+	buswrite(0x20); //inversion is off
 	comm1dat(0x0c, 0xe5); //set COLMOD
 	comm4dat(0x2a, 0x00, 0x00, 0x00, 0xef); //set column min-max
 	comm4dat(0x2b, 0x00, 0x00, 0x01, 0x3f); //set page min-max
-	comm(0x2c); //begin frame write
-	mark(&placeholder); //just keep dumping data into the frame infinitely
-	data(0x00); //red (upper 6 bits)
-	data(0xc8); //green (upper 6 bits)
-	data(0xd8); //blue (upper 6 bits)
+	buswrite(0x2c); //begin frame write
+	inst("imm out1 0400");
+	placeholder = addr; //just keep dumping data into the frame infinitely
+	buswrite(0x00); //red (upper 6 bits)
+	buswrite(0xc8); //green (upper 6 bits)
+	buswrite(0xd8); //blue (upper 6 bits)
 	jump(placeholder);
-	close(fd);
+	fclose(fd);
 }
