1a91baea866df644624febd44370228706bb3f33
Isaac Hair <isaacmhair@gmail.com>
2020-11-01 23:57:54 -0800

    troubleshooting the interface

--- lcd_arduino_tests/test11/test11.ino
+++ lcd_arduino_tests/test11/test11.ino
@@ -5,6 +5,7 @@
 // This guy is using inverted outputs because it is going thru the sheild
 // meant for use with map25.2 to test it
 // I am also going to modify the pinout to interface with the adapter to map25.2.
+// only data is inverted, not control pins
 //
  #define CS A2   // Chip Select goes to Analog 3
  #define CD A3   // Command/Data goes to Analog 2
@@ -24,119 +25,82 @@ void setup(void) {
   int x;
 // Set up port directions and default states
   pinMode(CS, OUTPUT);  pinMode(CD, OUTPUT);  pinMode(WR, OUTPUT);  pinMode(RD, OUTPUT);  pinMode(RST, OUTPUT);
-  digitalWrite(CS, 0); digitalWrite(CD, 0); digitalWrite(WR, 0); digitalWrite(RD, 0); digitalWrite(RST, 0);
+  digitalWrite(CS, 1); digitalWrite(CD, 1); digitalWrite(WR, 1); digitalWrite(RD, 1); digitalWrite(RST, 1);
   pinMode(D0, OUTPUT); pinMode(D1, OUTPUT); pinMode(D2, OUTPUT); pinMode(D3, OUTPUT);
   pinMode(D4, OUTPUT); pinMode(D5, OUTPUT); pinMode(D6, OUTPUT); pinMode(D7, OUTPUT);
   
 //HW Reset
-  digitalWrite(RST,1); delay(5);
-  digitalWrite(RST,0); delay(500);
+  digitalWrite(RST,0); delay(5);
+  digitalWrite(RST,1); delay(500);
 
   //set all parameters and get ready to write  
   pinMode(D0, OUTPUT);    pinMode(D1, OUTPUT);    pinMode(D2, OUTPUT);    pinMode(D3, OUTPUT);
   pinMode(D4, OUTPUT);    pinMode(D5, OUTPUT);    pinMode(D6, OUTPUT);    pinMode(D7, OUTPUT);
-  digitalWrite(RD, 0);
-  digitalWrite(CS, 0);
-  digitalWrite(WR, 0);
-  digitalWrite(CD, 0);
+  digitalWrite(RD, 1);
+  digitalWrite(CS, 1);
+  digitalWrite(WR, 1);
+  digitalWrite(CD, 1);
 
   //out of IDLE mode
-  digitalWrite(CS, 1);
-  digitalWrite(CD, 1);
+  digitalWrite(CS, 0);
+  digitalWrite(CD, 0);
   digitalWrite(D0, 1); digitalWrite(D1, 1); digitalWrite(D2, 1); digitalWrite(D3, 0);  // 8 - Change for lower nibble as required
   digitalWrite(D4, 0); digitalWrite(D5, 0); digitalWrite(D6, 1); digitalWrite(D7, 1);  // 3 - Change for upper nibble as required
-  digitalWrite(WR, 1); digitalWrite(WR, 0);
+  digitalWrite(WR, 0); digitalWrite(WR, 1);
 
   //out of SLEEP mode
   digitalWrite(D0, 0); digitalWrite(D1, 1); digitalWrite(D2, 1); digitalWrite(D3, 1);  // 1 - Change for lower nibble as required
   digitalWrite(D4, 0); digitalWrite(D5, 1); digitalWrite(D6, 1); digitalWrite(D7, 1);  // 1 - Change for upper nibble as required
-  digitalWrite(WR, 1); digitalWrite(WR, 0);
+  digitalWrite(WR, 0); digitalWrite(WR, 1);
   
   //display on mode
   digitalWrite(D0, 0); digitalWrite(D1, 0); digitalWrite(D2, 1); digitalWrite(D3, 1);  // 3 - Change for lower nibble as required
   digitalWrite(D4, 0); digitalWrite(D5, 1); digitalWrite(D6, 1); digitalWrite(D7, 1);  // 1 - Change for upper nibble as required
-  digitalWrite(WR, 1); digitalWrite(WR, 0);
+  digitalWrite(WR, 0); digitalWrite(WR, 1);
 
   //set COLMOD
   digitalWrite(D0, 1); digitalWrite(D1, 1); digitalWrite(D2, 0); digitalWrite(D3, 0);  // C - Change for lower nibble as required
   digitalWrite(D4, 1); digitalWrite(D5, 1); digitalWrite(D6, 1); digitalWrite(D7, 1);  // 0 - Change for upper nibble as required
-  digitalWrite(WR, 1); digitalWrite(WR, 0);
-  digitalWrite(CD, 0);
+  digitalWrite(WR, 0); digitalWrite(WR, 1);
+  digitalWrite(CD, 1);
   digitalWrite(D0, 1); digitalWrite(D1, 0); digitalWrite(D2, 0); digitalWrite(D3, 1);  // 6 - Change for lower nibble as required
   digitalWrite(D4, 1); digitalWrite(D5, 0); digitalWrite(D6, 0); digitalWrite(D7, 0);  // e - Change for upper nibble as required
-  digitalWrite(WR, 1); digitalWrite(WR, 0);
+  digitalWrite(WR, 0); digitalWrite(WR, 1);
 
   //invsersion off
-  digitalWrite(CD, 1);
+  digitalWrite(CD, 0);
   digitalWrite(D0, 1); digitalWrite(D1, 1); digitalWrite(D2, 1); digitalWrite(D3, 1);  // 0 - Change for lower nibble as required
   digitalWrite(D4, 1); digitalWrite(D5, 0); digitalWrite(D6, 1); digitalWrite(D7, 1);  // 2 - Change for upper nibble as required
-  digitalWrite(WR, 1); digitalWrite(WR, 0);
+  digitalWrite(WR, 0); digitalWrite(WR, 1);
 
   //turn memory to screen on
   digitalWrite(D0, 0); digitalWrite(D1, 1); digitalWrite(D2, 1); digitalWrite(D3, 0);  // 9 - Change for lower nibble as required
   digitalWrite(D4, 1); digitalWrite(D5, 0); digitalWrite(D6, 1); digitalWrite(D7, 1);  // 2 - Change for upper nibble as required
-  digitalWrite(WR, 1); digitalWrite(WR, 0);
-
-  /*//column address
-  digitalWrite(D0, 1); digitalWrite(D1, 0); digitalWrite(D2, 1); digitalWrite(D3, 0);  // A - Change for lower nibble as required
-  digitalWrite(D4, 1); digitalWrite(D5, 0); digitalWrite(D6, 1); digitalWrite(D7, 1);  // 2 - Change for upper nibble as required
-  digitalWrite(WR, 1); digitalWrite(WR, 0);
-  digitalWrite(CD, 0);
-  digitalWrite(D0, 1); digitalWrite(D1, 1); digitalWrite(D2, 1); digitalWrite(D3, 1);  // 0 - Change for lower nibble as required
-  digitalWrite(D4, 1); digitalWrite(D5, 1); digitalWrite(D6, 1); digitalWrite(D7, 1);  // 0 - Change for upper nibble as required
-  digitalWrite(WR, 1); digitalWrite(WR, 0);
-  digitalWrite(D0, 1); digitalWrite(D1, 1); digitalWrite(D2, 1); digitalWrite(D3, 1);  // 0 - Change for lower nibble as required
-  digitalWrite(D4, 1); digitalWrite(D5, 1); digitalWrite(D6, 1); digitalWrite(D7, 1);  // 0 - Change for upper nibble as required
-  digitalWrite(WR, 1); digitalWrite(WR, 0);
-  digitalWrite(D0, 1); digitalWrite(D1, 1); digitalWrite(D2, 1); digitalWrite(D3, 1);  // 0 - Change for lower nibble as required
-  digitalWrite(D4, 1); digitalWrite(D5, 1); digitalWrite(D6, 1); digitalWrite(D7, 1);  // 0 - Change for upper nibble as required
-  digitalWrite(WR, 1); digitalWrite(WR, 0);
-  digitalWrite(D0, 0); digitalWrite(D1, 0); digitalWrite(D2, 0); digitalWrite(D3, 0);  // f - Change for lower nibble as required
-  digitalWrite(D4, 1); digitalWrite(D5, 0); digitalWrite(D6, 0); digitalWrite(D7, 0);  // e - Change for upper nibble as required
-  digitalWrite(WR, 1); digitalWrite(WR, 0);
-
-  //page address
-  digitalWrite(CD, 1);
-  digitalWrite(D0, 0); digitalWrite(D1, 0); digitalWrite(D2, 1); digitalWrite(D3, 0);  // B - Change for lower nibble as required
-  digitalWrite(D4, 1); digitalWrite(D5, 0); digitalWrite(D6, 1); digitalWrite(D7, 1);  // 2 - Change for upper nibble as required
-  digitalWrite(WR, 1); digitalWrite(WR, 0);
-  digitalWrite(CD, 0);
-  digitalWrite(D0, 1); digitalWrite(D1, 1); digitalWrite(D2, 1); digitalWrite(D3, 1);  // 0 - Change for lower nibble as required
-  digitalWrite(D4, 1); digitalWrite(D5, 1); digitalWrite(D6, 1); digitalWrite(D7, 1);  // 0 - Change for upper nibble as required
-  digitalWrite(WR, 1); digitalWrite(WR, 0);
-  digitalWrite(D0, 1); digitalWrite(D1, 1); digitalWrite(D2, 1); digitalWrite(D3, 1);  // 0 - Change for lower nibble as required
-  digitalWrite(D4, 1); digitalWrite(D5, 1); digitalWrite(D6, 1); digitalWrite(D7, 1);  // 0 - Change for upper nibble as required
-  digitalWrite(WR, 1); digitalWrite(WR, 0);
-  digitalWrite(D0, 0); digitalWrite(D1, 1); digitalWrite(D2, 1); digitalWrite(D3, 1);  // 1 - Change for lower nibble as required
-  digitalWrite(D4, 1); digitalWrite(D5, 1); digitalWrite(D6, 1); digitalWrite(D7, 1);  // 0 - Change for upper nibble as required
-  digitalWrite(WR, 1); digitalWrite(WR, 0);
-  digitalWrite(D0, 0); digitalWrite(D1, 0); digitalWrite(D2, 0); digitalWrite(D3, 0);  // f - Change for lower nibble as required
-  digitalWrite(D4, 0); digitalWrite(D5, 0); digitalWrite(D6, 1); digitalWrite(D7, 1);  // 3 - Change for upper nibble as required
-  digitalWrite(WR, 1); digitalWrite(WR, 0);*/
+  digitalWrite(WR, 0); digitalWrite(WR, 1);
 
   //write that square to green
-  digitalWrite(CD, 1);
+  digitalWrite(CD, 0);
   digitalWrite(D0, 1); digitalWrite(D1, 1); digitalWrite(D2, 0); digitalWrite(D3, 0);  // C - Change for lower nibble as required
   digitalWrite(D4, 1); digitalWrite(D5, 0); digitalWrite(D6, 1); digitalWrite(D7, 1);  // 2 - Change for upper nibble as required
-  digitalWrite(WR, 1); digitalWrite(WR, 0);
-  digitalWrite(CD, 0);
+  digitalWrite(WR, 0); digitalWrite(WR, 1);
+  digitalWrite(CD, 1);
   for (int i = 0; i < 320; i++)
     for (int j = 0; j < 240; j++) {
       digitalWrite(D0, 1); digitalWrite(D1, 1); digitalWrite(D2, 1); digitalWrite(D3, 1);  // 0 - Change for lower nibble as required
       digitalWrite(D4, 1); digitalWrite(D5, 1); digitalWrite(D6, 1); digitalWrite(D7, 1);  // 0 - Change for upper nibble as required
-      digitalWrite(WR, 1); digitalWrite(WR, 0);
+      digitalWrite(WR, 0); digitalWrite(WR, 1);
       digitalWrite(D0, 1); digitalWrite(D1, 1); digitalWrite(D2, 1); digitalWrite(D3, 1);  // 0 - Change for lower nibble as required
       digitalWrite(D4, 1); digitalWrite(D5, 1); digitalWrite(D6, 0); digitalWrite(D7, 1);  // 4 - Change for upper nibble as required
-      digitalWrite(WR, 1); digitalWrite(WR, 0);
+      digitalWrite(WR, 0); digitalWrite(WR, 1);
       digitalWrite(D0, 1); digitalWrite(D1, 1); digitalWrite(D2, 1); digitalWrite(D3, 1);  // 0 - Change for lower nibble as required
       digitalWrite(D4, 1); digitalWrite(D5, 1); digitalWrite(D6, 1); digitalWrite(D7, 0);  // 8 - Change for upper nibble as required
-      digitalWrite(WR, 1); digitalWrite(WR, 0);
+      digitalWrite(WR, 0); digitalWrite(WR, 1);
     }
-  digitalWrite(CD, 1);
+  digitalWrite(CD, 0);
   digitalWrite(D0, 1); digitalWrite(D1, 1); digitalWrite(D2, 1); digitalWrite(D3, 1);  // 0 - Change for lower nibble as required
   digitalWrite(D4, 1); digitalWrite(D5, 1); digitalWrite(D6, 1); digitalWrite(D7, 1);  // 0 - Change for upper nibble as required
-  digitalWrite(WR, 1); digitalWrite(WR, 0);
-  digitalWrite(CD, 0);
+  digitalWrite(WR, 0); digitalWrite(WR, 1);
+  digitalWrite(CD, 1);
   
 }
 //Nothing to do here except ur mom but already did her so...
