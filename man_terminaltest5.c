8e1a627b71c177ea368c6f58df70994545afc452
IsaacHair <isaacmhair@gmail.com>
2020-11-06 00:58:01 -0800

    liked the other colors better

--- man_terminaltest5.c
+++ man_terminaltest5.c
@@ -89,9 +89,10 @@ iterate:
 	dotwocomp(&zis, zis);
 	calladd(&zr, zrs, zis);
 	calladd(&zr, zr, cr);
+	i += 1; //putting this here instead of after checking zr
+		//shifts the colors by 1; <4 means <3 iterations
 	if (zr > 2.0 || zi > 2.0 || zr < -2.0 || zi < -2.0)
 		goto iterate_end;
-	i += 1;
 	if (i < 32)
 		goto iterate;
 iterate_end:
