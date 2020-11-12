b15703459550b7e720c026282f29acb004c1a871
IsaacHair <isaacmhair@gmail.com>
2020-11-11 23:51:43 -0800

    should be working predecessor, but it is untested

--- mandelbrot_attempt_1/genpred00.c
+++ mandelbrot_attempt_1/genpred00.c
@@ -37,7 +37,7 @@ void setprevnxt(unsigned short nxt) {
 	do
 		fseek(fd, -2, SEEK_CUR);
 	while (fgetc(fd) != ' ');
-	fprintf("%04x\n", nxt);
+	fprintf(fd, "%04x\n", nxt);
 }
 
 void makeaddrodd() {
@@ -51,7 +51,38 @@ void makeaddrodd() {
 void genpred16() {
 	//note: only the previous instruction in the file
 	//should feed to this macro
-	unsigned short mask;
+	//note: this guy ends on an instruction with a shitload
+	//of pointers to it
+	//note: needs previous instruction
+	unsigned short mask, addrshift;
+	int i;
 	
 	makeaddrodd();
-	instaddr(
+	for (i = 16; i >= 2; i = i>>1)
+		for (mask = 0xffff>>(i/2), addrshift = 16/i;
+		     mask; mask = mask>>i, addrshift++)
+			instvalnxt("gen jzor", mask, addr+addrshift);
+	instvalnxt("gen jzor", 0xffff, addr+16);
+	for (mask = 0x3fff; mask; mask = mask>>1)
+		instvalnxt("imm gen1", mask, addr+19);
+	instvalnxt("imm gen0", 0x0001, addr+19);
+	instvalnxt("imm gen1", 0xffff, addr+18);
+	instvalnxt("imm gen1", 0x7fff, addr+1);
+	for (mask = 0x8000, i = 16; mask >= 0x0001; mask = mask>>1, i--)
+		instvalnxt("imm gen0", mask, addr+i);
+}
+
+void main(int argc, char**argv) {
+	if (argc != 2) {
+		printf("need target\n");
+		exit(0x01);
+	}
+	fd = fopen(argv[1], "w");
+	addr = 0;
+	
+	inst("dnc noop 0000");
+	genpred16();
+	instnxt("dnc noop 0000", 0x0000);
+
+	fclose(fd);
+}
