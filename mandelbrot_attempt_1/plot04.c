0f4797f8a56fcae439ad84af59be563203754591
Isaac Hair <isaacmhair@gmail.com>
2020-11-15 04:01:53 -0800

    updated comment at top of plot04.c because IT WORKS

--- mandelbrot_attempt_1/plot04.c
+++ mandelbrot_attempt_1/plot04.c
@@ -2,12 +2,10 @@
 #include <stdlib.h>
 
 /*
- * This will simply display some test multiplications to the LED displays.
- * Addr and out will display the factors and dir will display the product.
+ * Creates a rendering of the mandelbrot set.
+ * Integers are simply stored as 16 bit integers.
+ * "Floating points" are stored as follows:
  * The upper bit is the sign, 3 after that are int part, 12 after are fraction.
- * Gen is used to to count up and delay the output so that the test
- * numbers are displayed for like 1 second before being changed to the next
- * pair.
  * Note: this program expects \n to constitute only ONE character;
  * this is relevant when re-writing nxt addresses.
  * Note: FUNCTIONS DO NOT SUPPORT RECURSION.
