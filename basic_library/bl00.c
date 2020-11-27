b30cb773a0077f021b52b8c8feffb5de9d82e657
Isaac Hair <isaacmhair@gmail.com>
2020-11-27 13:51:16 -0800

    should be replacing the stuff now

--- basic_library/bl00.c
+++ basic_library/bl00.c
@@ -5,35 +5,71 @@ unsigned short addr;
 FILE* fd;
 
 void inst(char*op) {
-	fprintf(fd, "%04x %s %04x\n", addr, op, addr+1);
+	fprintf(fd, "%04x %s \x88%04x\n", addr, op, addr+1);
 	addr++;
 }
 
 void instval(char*op, unsigned short val) {
-	fprintf(fd, "%04x %s %04x %04x\n", addr, op, val, addr+1);
+	fprintf(fd, "%04x %s %04x \x88%04x\n", addr, op, val, addr+1);
 	addr++;
 }
 
 void instnxt(char*op, unsigned short nxt) {
-	fprintf(fd, "%04x %s %04x\n", addr, op, nxt);
+	fprintf(fd, "%04x %s \x88%04x\n", addr, op, nxt);
 	addr++;
 }
 
 void instvalnxt(char*op, unsigned short val, unsigned short nxt) {
-	fprintf(fd, "%04x %s %04x %04x\n", addr, op, val, nxt);
+	fprintf(fd, "%04x %s %04x \x88%04x\n", addr, op, val, nxt);
 	addr++;
 }
 
+void replaceaddr(char* old, char* new) {
+	//leaves \x88 identifier
+	int c;
+	char str[4];
+	int copy;
+	fseek(fd, 0, SEEK_SET);
+	for (c = fgetc(fd), copy = -1; c != EOF; c = fgetc(fd)) {
+		if (c == 0x88) {
+			copy = 0;
+			continue;
+		}
+		if (copy > -1) {
+			str[copy] = c;
+			copy++;
+		}
+		if (copy == 4) {
+			copy = -1;
+			if (str[0] == old[0] && str[1] == old[1]
+			    && str[2] == old[2] && str[3] == old[3]) {
+				fseek(fd, -4, SEEK_CUR);
+				fwrite(new, sizeof(char), 4, fd);
+			}
+		}
+	}
+}
+
+void makeaddrodd() {
+	char old[5];
+	char new[5];
+	if (addr%2 == 0) {
+		sprintf(old, "%04x", addr);
+		sprintf(new, "%04x", addr+1);
+		replaceaddr(old, new);
+		addr++;
+	}
+}
+
+void removex88() {
+	//remove that stuff
+}
+
 void addrpred16() {
-	//note: only the previous instruction in the file
-	//should feed to this macro
-	//note: this guy ends on an instruction with a shitload
-	//of pointers to it
-	//note: needs previous instruction
 	unsigned short mask, addrshift;
 	int i;
 	
-/**/	makeaddrodd();
+	makeaddrodd();
 	for (i = 16; i >= 2; i = i>>1)
 		for (mask = 0xffff>>(i/2), addrshift = 16/i;
 		     mask; mask = mask>>i, addrshift++)
@@ -49,15 +85,10 @@ void addrpred16() {
 }
 
 void genpred16() {
-	//note: only the previous instruction in the file
-	//should feed to this macro
-	//note: this guy ends on an instruction with a shitload
-	//of pointers to it
-	//note: needs previous instruction
 	unsigned short mask, addrshift;
 	int i;
 	
-/**/	makeaddrodd();
+	makeaddrodd();
 	for (i = 16; i >= 2; i = i>>1)
 		for (mask = 0xffff>>(i/2), addrshift = 16/i;
 		     mask; mask = mask>>i, addrshift++)
@@ -73,5 +104,17 @@ void genpred16() {
 }
 
 void main(int argc, char** argv) {
-	//test code
+	if (argc != 3) {
+		printf("need buffer, target\n");
+		exit(0x01);
+	}
+	addr = 0;
+	fd = fopen(argv[2], "w+");
+
+	inst("dnc noop 0000");
+	inst("dnc noop 0000");
+	addrpred16();
+	genpred16();
+
+	removex88();
 }
