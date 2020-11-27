a62d1dadf8f2bb0edcb21ab806099e5dbecbb92c
Isaac Hair <isaacmhair@gmail.com>
2020-11-27 00:47:41 -0800

    creating some basic stock functions

---
+++ basic_library/bl00.c
@@ -0,0 +1,77 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+unsigned short addr;
+FILE* fd;
+
+void inst(char*op) {
+	fprintf(fd, "%04x %s %04x\n", addr, op, addr+1);
+	addr++;
+}
+
+void instval(char*op, unsigned short val) {
+	fprintf(fd, "%04x %s %04x %04x\n", addr, op, val, addr+1);
+	addr++;
+}
+
+void instnxt(char*op, unsigned short nxt) {
+	fprintf(fd, "%04x %s %04x\n", addr, op, nxt);
+	addr++;
+}
+
+void instvalnxt(char*op, unsigned short val, unsigned short nxt) {
+	fprintf(fd, "%04x %s %04x %04x\n", addr, op, val, nxt);
+	addr++;
+}
+
+void addrpred16() {
+	//note: only the previous instruction in the file
+	//should feed to this macro
+	//note: this guy ends on an instruction with a shitload
+	//of pointers to it
+	//note: needs previous instruction
+	unsigned short mask, addrshift;
+	int i;
+	
+/**/	makeaddrodd();
+	for (i = 16; i >= 2; i = i>>1)
+		for (mask = 0xffff>>(i/2), addrshift = 16/i;
+		     mask; mask = mask>>i, addrshift++)
+			instvalnxt("addr jzor", mask, addr+addrshift);
+	instvalnxt("addr jzor", 0xffff, addr+16);
+	for (mask = 0x3fff; mask; mask = mask>>1)
+		instvalnxt("imm addr1", mask, addr+18);
+	instvalnxt("imm addr0", 0x0001, addr+18);
+	instvalnxt("imm addr1", 0xffff, addr+17);
+	instvalnxt("imm addr1", 0x7fff, addr+1);
+	for (mask = 0x8000, i = 15; mask >= 0x0002; mask = mask>>1, i--)
+		instvalnxt("imm addr0", mask, addr+i);
+}
+
+void genpred16() {
+	//note: only the previous instruction in the file
+	//should feed to this macro
+	//note: this guy ends on an instruction with a shitload
+	//of pointers to it
+	//note: needs previous instruction
+	unsigned short mask, addrshift;
+	int i;
+	
+/**/	makeaddrodd();
+	for (i = 16; i >= 2; i = i>>1)
+		for (mask = 0xffff>>(i/2), addrshift = 16/i;
+		     mask; mask = mask>>i, addrshift++)
+			instvalnxt("gen jzor", mask, addr+addrshift);
+	instvalnxt("gen jzor", 0xffff, addr+16);
+	for (mask = 0x3fff; mask; mask = mask>>1)
+		instvalnxt("imm gen1", mask, addr+18);
+	instvalnxt("imm gen0", 0x0001, addr+18);
+	instvalnxt("imm gen1", 0xffff, addr+17);
+	instvalnxt("imm gen1", 0x7fff, addr+1);
+	for (mask = 0x8000, i = 15; mask >= 0x0002; mask = mask>>1, i--)
+		instvalnxt("imm gen0", mask, addr+i);
+}
+
+void main(int argc, char** argv) {
+	//test code
+}
---
+++ basic_library/readme.txt
@@ -0,0 +1,5 @@
+This collection of c functions will define extremely simple things,
+ie. assembly instruction lines and how to go back and forth to replace
+placeholders with their actual values (for things like goto and if-else).
+
+Just copy paste the functions, don't include the entire file as a library.
