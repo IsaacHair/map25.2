a1aa00eed63a20f7700481e22eed7c0400213869
IsaacHair <isaacmhair@gmail.com>
2020-12-02 00:59:29 -0800

    it is working, just has printfs

--- map25.2_simulator/convert01.c
+++ map25.2_simulator/convert01.c
@@ -1,23 +1,106 @@
+//Expects good formatting for the input file
+//Simple state machine
+//All states grab the next character for the next state
+//This design might create stack overflow issues.
+
 #include <stdlib.h>
 #include <stdio.h>
+
+FILE* source;
+FILE* target;
+int page;
+int data;
+int line;
+int chip;
+char program[655360];
+int c;
+
+#define fgetc(source) fgetc(source+0); printf("char:%c\n", c);
+
+void nextchipindicator() {
+	chip++;
+
+	c = fgetc(source);
+	switch(c) {
+		case EOF:
+			break;
+		default:
+			pageaddress();
+			break;
+	}
+}
+
+void nextpageindicator() {
+	c = fgetc(source);
+	switch(c) {
+		case 'z':
+			nextchipindicator();
+			break;
+		default:
+			pageaddress();
+			break;
+	}
+}
+
+void lineanddata() {
+	int shift, i;
+
+	line = 0;
+	for (shift = 1<<4; shift; shift = shift>>4) {
+		if (c >= '0' && c <= '9')
+			line |= (c-'0')*shift;
+		else
+			line |= (c-'0'+10)*shift;
+		c = fgetc(source);
+	}
+	for (i = 0; i < 2; i++) {
+		printf("lineanddata @%x,%x(%x):%c\n", page, line, ((page&0xff80)|(line&0x007f))*10+8-(2*chip)+i, c);
+		program[((page&0xff80)|(line&0x007f))*10+8-(2*chip)+i] = c;
+		c = fgetc(source);
+	}
+
+	switch (c) {
+		case 'x':
+			nextpageindicator();
+			break;
+		default:
+			lineanddata();
+			break;
+	}
+}
+
+void pageaddress() {
+	int shift;
+
+	page = 0x0000;
+	for (shift = 1<<12; shift; shift = shift>>4) {
+		if (c >= '0' && c <= '9')
+			page |= (c-'0')*shift;
+		else
+			page |= (c-'a'+10)*shift;
+		c = fgetc(source);
+	}
+	printf("page:%x\n", page);
+
+	lineanddata();
+}
 
 void main(int argc, char** argv) {
 	if (argc != 3) {
 		printf("need <source> <target>");
 		exit (0x01);
 	}
-	FILE* source;
-	FILE* target;
-	char program[655360];
 	int i;
 
 	source = fopen(argv[1], "r");
 	target = fopen(argv[2], "w");
+	chip = 0;
 
 	for (i = 0; i < 655360; i++)
 		program[i] = 'f';
 
-	for (
+	c = fgetc(source);
+	pageaddress();
 
 	for (i = 0; i < 655360; i++)
 		fprintf(target, "%c", program[i]);
