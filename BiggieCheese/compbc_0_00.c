7b35142bb8553d42990d03714b2008821c9e9bc8
Isaac Hair <isaacmhair@gmail.com>
2021-02-14 03:22:23 -0800

    defined struct* thingy

--- BiggieCheese/compbc_0_00.c
+++ BiggieCheese/compbc_0_00.c
@@ -1,8 +1,24 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void main(int argc, char** argv) {
-	
+struct line {
+	char* content;
+	struct line *nested;
+	struct line *next;
 }
 
-void ramparse(
+void main(int argc, char** argv) {
+	//note that everything is de-allocated when the program stops,
+	//so don't have to be anal about garbage collection
+	
+	struct line program;
+	FILE* source;
+	if (argc != 2) {
+		printf("need source file\n");
+		exit(0x01);
+	}
+	ramparse (source, *program);
+}
+
+//this bad boy just does some very basic opening etc
+void ramparse(FILE* source, struct line *program) {}
